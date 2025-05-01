ed8116d614f627a2e0594d7a58880608697d4fec
nattarat016 <nattaratthepphoy@gmail.com>
2025-05-01 10:40:16 +0700

    1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://writer-assistant-pied.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
