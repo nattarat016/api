045caeef526a4fa768bf7dd9bafdc68dbbd60aa6
nattarat016 <nattaratthepphoy@gmail.com>
2025-05-01 09:46:53 +0700

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class AnalyzeRequest(BaseModel):
+    novelId: str | None
+    chapterId: str | None
+    content: str
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello Backend!"}
+
+@app.post("/analyze")
+def analyze_content(data: AnalyzeRequest):
+    return {
+        "message": "Analyze successful!",
+        "novelId": data.novelId,
+        "chapterId": data.chapterId,
+        "content": data.content
+    }
